75790b014b1f4551f5bb6571c040c4cc60293e8d
mariem kamaych <kamaychmeryem@gmail.com>
2024-01-17 11:04:19 +0100

    add sign up method and user model and routes

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
-
+const userRoute = require('./routes/authRoutes');
 const mongoose = require('mongoose');
+require('dotenv').config()
+
+app.use(express.json());
+
 
 const port = 4000;
 const db = "mongodb://127.0.0.1:27017/booking-trip"
@@ -14,3 +18,6 @@ mongoose.connect(db)
 app.listen(port,()=>{
     console.log(`app is listning to ${port}`);
 })
+
+ 
+app.use('/api/auth',userRoute);
